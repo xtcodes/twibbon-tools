60c231e1d4baa2142cf89efd9cedcbdfb5cf1539
Ferry Ayunda <ayunda.fa0604@gmail.com>
2025-06-05 00:00:05 +0700

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('twibbon-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png',
+        './twibbon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
